e0a7d36f8d370da42fa82dc435f17c3ac70a148c
Yujing Zheng <hzyh64160@gmail.com>
2014-03-02 16:30:06 -0600

    clean up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require jquery-1.8.2.min
 //= require jquery_ujs
 //= require underscore
 //= require backbone
@@ -18,4 +18,3 @@
 //= require backbone_datalink
 //= require backbone/app_finder
 //= require_tree .
-
